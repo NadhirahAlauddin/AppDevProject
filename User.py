b9182f535123465c9dfddab9b41df39a359d9267
Nad <214118M@mymail.nyp.edu.sg>
2023-02-06 23:50:55 +0800

    Modified attributes in User class

--- User.py
+++ User.py
@@ -1,48 +1,67 @@
-# User class, Changed name from User to Register
-class Register:
+# User class
+class User:
     count_id = 0
 
     # initializer method
-    def __init__(self, email_address, username, password, confirm_password):
-        Register.count_id += 1
-        self.__register_id = Register.count_id
-        self.__email_address = email_address
+    # Details needed are Email_add, username, Fname, lname, pwd, confirm pwd, gender)
+    def __init__(self, first_name, last_name, gender, email_add, username, pwd, confirm_pwd):
+        User.count_id += 1
+        self.__user_id = User.count_id
+        self.__first_name = first_name
+        self.__last_name = last_name
+        self.__gender = gender
+        self.__email_add = email_add
         self.__username = username
-        self.__password = password
-        self.__confirm_password = confirm_password
+        self.__pwd = pwd
+        self.__confirm_pwd = confirm_pwd
 
 
     # accessor methods
-    def get_register_id(self):
-        return self.__register_id
+    def get_user_id(self):
+        return self.__user_id
 
-    def get_email_address(self):
-        return self.__email_address
+    def get_first_name(self):
+        return self.__first_name
+
+    def get_last_name(self):
+        return self.__last_name
+
+    def get_gender(self):
+        return self.__gender
+
+    def get_email_add(self):
+        return self.__email_add
 
     def get_username(self):
         return self.__username
 
-    def get_password(self):
-        return self.__password
+    def get_pwd(self):
+        return self.__pwd
 
-    def get_confirm_password(self):
-        return self.__confirm_password
-
+    def get_confirm_pwd(self):
+        return self.__confirm_pwd
 
     # mutator methods
-    def set_register_id(self, register_id):
-        self.__register_id = register_id
+    def set_user_id(self, user_id):
+        self.__user_id = user_id
 
-    def set_email_address(self, email_address):
-        self.__email_address = email_address
+    def set_first_name(self, first_name):
+        self.__first_name = first_name
+
+    def set_last_name(self, last_name):
+        self.__last_name = last_name
+
+    def set_gender(self, gender):
+        self.__gender = gender
+
+    def set_email_add(self, email_add):
+        self.__email_add = email_add
 
     def set_username(self, username):
         self.__username = username
 
-    def set_password(self, password):
-        self.__password = password
+    def set_pwd(self, pwd):
+        self.__pwd = pwd
 
-    def set_confirm_password(self, confirm_password):
-        self.__confirm_password = confirm_password
-
-
+    def set_confirm_pwd(self, confirm_pwd):
+        self.__confirm_pwd = confirm_pwd
