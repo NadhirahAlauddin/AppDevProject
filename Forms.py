17deef589e23a935ff8c4693d78aa239f7ea099b
Nad <214118M@mymail.nyp.edu.sg>
2023-02-06 23:49:56 +0800

    Modified RegisterForm class

--- Forms.py
+++ Forms.py
@@ -1,5 +1,5 @@
-from wtforms import Form, StringField, RadioField, SelectField, TextAreaField, validators, PasswordField, SubmitField
-from wtforms.fields import EmailField, DateField
+from wtforms import Form, StringField, SelectField, TextAreaField, validators, PasswordField
+from wtforms.fields import EmailField
 from flask_wtf import FlaskForm
 import shelve
 import re
@@ -30,16 +30,14 @@ def validate_existing_username(form, field):
 
 
 
-class RegisterForm(FlaskForm):
-     email_address = EmailField('Email Address:',
-                                [validators.Email(), validators.DataRequired(), validate_existing_email])
-     username = StringField('Username:',
-                            [validators.Length(min=1, max=150), validators.DataRequired(), validate_existing_username])
-     password = PasswordField('Password:',
-                              [validators.Length(min=8, max=150), validators.InputRequired(), validate_password])
-     confirm_password = PasswordField('Confirm Password:',
-                                      [validators.EqualTo('password', message='Passwords must match'),
-                                       validators.InputRequired()])
+class RegisterForm(Form):
+     email_address = EmailField('Email Address:',[validators.Email(), validators.DataRequired(), validators.DataRequired()]) #validate_existing_email was replaced with validators.DataRequired
+     username = StringField('Username:',[validators.Length(min=1, max=150), validators.DataRequired(), validators.DataRequired() ]) #validate_existing_username was replaced with validators.DataRequired
+     first_name = StringField('First Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+     last_name = StringField('Last Name', [validators.Length(min=1, max=150), validators.DataRequired()])
+     gender = SelectField('Gender', [validators.DataRequired()],choices=[('', 'Select'), ('F', 'Female'), ('M', 'Male')], default='')
+     password = PasswordField('Password:',[validators.Length(min=8, max=150), validators.InputRequired(), validate_password])
+     confirm_password = PasswordField('Confirm Password:',[validators.EqualTo('password', message='Passwords must match'),validators.InputRequired()])
 
 class LoginForm(FlaskForm):
     login_username = StringField('Username:', [validators.DataRequired(), validate_existing_username])
