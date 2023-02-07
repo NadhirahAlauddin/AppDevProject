800cbe99bd12ef55ebadce7687bbfdcbc6ae34db
Nad <214118M@mymail.nyp.edu.sg>
2023-02-07 09:00:06 +0800

    Added retrieveUsers & updateUser route

--- __init__.py
+++ __init__.py
@@ -20,21 +20,21 @@ def login():
 #
 
 @app.route('/register', methods=['GET', 'POST'])
-def register():
+def register_user():
     register_form = RegisterForm(request.form)
     if request.method == 'POST' and register_form.validate():
-        registered_dict = {}
-        db = shelve.open('registered.db', 'c')
+        regUsers_dict = {}
+        db = shelve.open('Users.db', 'c')
 
         try:
-            registered_dict = db['Registered Users']
+            regUsers_dict = db['Registered Users']
         except:
-            print("Error in retrieving Registered Users from registered.db.")
+            print("Error in retrieving Registered Users from Users.db.")
 
-        register = User.Register(register_form.email_address.data, register_form.username.data,
-                         register_form.password.data, register_form.confirm_password.data)
-        registered_dict[register.get_register_id()] = register
-        db['Registered Users'] = registered_dict
+        register_users = User.User(register_form.first_name.data, register_form.last_name.data,
+                         register_form.gender.data, register_form.email_add.data, register_form.username.data, register_form.password.data, register_form.confirm_password.data)
+        regUsers_dict[register_users.get_user_id()] = register_users
+        db['Registered Users'] = regUsers_dict
 
         db.close()
 
@@ -43,55 +43,57 @@ def register():
 
 
 
-# @app.route('/retrieveUsers')
-# def retrieve_users():
-#     users_dict = {}
-#     db = shelve.open('user.db', 'r')
-#     users_dict = db['Users']
-#     db.close()
+@app.route('/retrieveUsers')
+def retrieve_users():
+    regUsers_dict = {}
+    db = shelve.open('Users.db', 'r')
+    regUsers_dict = db['Registered Users']
+    db.close()
+
+    regUsers_list = []
+    for key in regUsers_dict:
+        register_users = regUsers_dict.get(key)
+        regUsers_list.append(register_users)
+
+    return render_template('retrieveUsers.html', count=len(regUsers_list), regUsers_dict=regUsers_list)
 #
-#     users_list = []
-#     for key in users_dict:
-#         user = users_dict.get(key)
-#         users_list.append(user)
-#
-#     return render_template('retrieveUsers.html', count=len(users_list), users_list=users_list)
 #
 #
-#
-# @app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
-# def update_user(id):
-#     update_user_form = CreateUserForm(request.form)
-#     if request.method == 'POST' and update_user_form.validate():
-#         users_dict = {}
-#         db = shelve.open('user.db', 'w')
-#         users_dict = db['Users']
-#
-#         user = users_dict.get(id)
-#         user.set_first_name(update_user_form.first_name.data)
-#         user.set_last_name(update_user_form.last_name.data)
-#         user.set_gender(update_user_form.gender.data)
-#         user.set_membership(update_user_form.membership.data)
-#         user.set_remarks(update_user_form.remarks.data)
-#
-#         db['Users'] = users_dict
-#         db.close()
-#
-#         return redirect(url_for('retrieve_users'))
-#     else:
-#         users_dict = {}
-#         db = shelve.open('user.db', 'r')
-#         users_dict = db['Users']
-#         db.close()
-#
-#         user = users_dict.get(id)
-#         update_user_form.first_name.data = user.get_first_name()
-#         update_user_form.last_name.data = user.get_last_name()
-#         update_user_form.gender.data = user.get_gender()
-#         update_user_form.membership.data = user.get_membership()
-#         update_user_form.remarks.data = user.get_remarks()
-#
-#         return render_template('updateUser.html', form=update_user_form)
+@app.route('/updateUser/<int:id>/', methods=['GET', 'POST'])
+def update_user(id):
+    update_user_form = RegisterForm(request.form)
+    if request.method == 'POST' and update_user_form.validate():
+        regUsers_dict = {}
+        db = shelve.open('Users.db', 'w')
+        regUsers_dict = db['Users']
+
+        user = regUsers_dict.get(id)
+        user.set_first_name(update_user_form.first_name.data)
+        user.set_last_name(update_user_form.last_name.data)
+        user.set_email_add(update_user_form.email_add.data)
+        user.set_username(update_user_form.username.data)
+        user.set_gender(update_user_form.gender.data)
+        user.set_pwd(update_user_form.password.data)
+        user.set_confirm_pwd(update_user_form.confirm_password.data)
+
+        db['Users'] = regUsers_dict
+        db.close()
+
+        return redirect(url_for('retrieve_users'))
+    else:
+        regUsers_dict = {}
+        db = shelve.open('user.db', 'r')
+        regUsers_dict = db['Users']
+        db.close()
+
+        user = regUsers_dict.get(id)
+        update_user_form.first_name.data = user.get_first_name()
+        update_user_form.last_name.data = user.get_last_name()
+        update_user_form.email_add.data = user.get_email_add()
+        update_user_form.username.data = user.get_username()
+        update_user_form.password.data = user.get_pwd()
+        update_user_form.confirm_password.data = user.get_confirm_pwd()
+        return render_template('updateUser.html', form=update_user_form)
 
 
 
